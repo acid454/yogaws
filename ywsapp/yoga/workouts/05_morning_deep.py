f6b0a088c45a8d08c8fdd9e0cf810fc86991c85d
Dmitry Repnikov <acid454@gmail.com>
2023-11-18 11:54:39 +0300

    Deep morning training

---
+++ ywsapp/yoga/workouts/05_morning_deep.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  05 morning_deep.py
+#  
+#  Copyright 2023 Dmitry Repnikov <acid454@x220>
+#  
+
+from dataclasses import dataclass
+from base import BaseWorkout
+from asanas import Asanas
+
+
+@dataclass
+class DefaultWorkout(BaseWorkout):
+    name: str = "morning_deep"
+    caption: str = "Утренняя медленная"
+    description: str = "Медленная утренняя тренровка из базовых асан"
+    
+    def __post_init__(self):
+        self.wrap_asana(Asanas.tadasana.Tadasana())
+        self.wrap_asana(Asanas.vitjashenie_vverh.VitjashenieVverh(tm_main = 20))
+        self.wrap_asana(Asanas.vitjashenie_vpered.VitjashenieVpered(tm_main = 10))
+        self.wrap_asana(Asanas.uttanasana.UttanasanaWithCompensation(tm_main = 40, tm_compensation = 20))
+
+        self.wrap_asana(Asanas.planka.Planka(tm_main = 60))
+        self.wrap_asana(Asanas.gorka.GorkaNormal(tm_main = 30, tm_prepare = 5))
+        self.wrap_asana(Asanas.virabhadrasana.VirabhadrasanaLeft(tm_main = 40))
+        self.wrap_asana(Asanas.gorka.GorkaBase(tm_main = 20))
+        self.wrap_asana(Asanas.virabhadrasana.VirabhadrasanaRight(tm_main = 40))
+        
+        self.wrap_asana(Asanas.gorka.GorkaBase(tm_main = 20))
+        self.wrap_asana(Asanas.sobaka_mordoi_vverh.SobakaMordoiVverh(tm_main = 30))
+        
+        self.wrap_asana(Asanas.gorka.GorkaBase(tm_main = 10))
+        self.wrap_asana(Asanas.kapotasana.Kapotasana())
+
+        self.wrap_asana(Asanas.gorka.GorkaBase(tm_main = 10))
+        self.wrap_asana(Asanas.short_poses.Seli())
+        self.wrap_asana(Asanas.perekati_na_spine.Perekatu_na_spine())
+        self.wrap_asana(Asanas.plug.Plug())
+        self.wrap_asana(Asanas.sarvangasana.Sarvangasana())
+        self.wrap_asana(Asanas.shavasana.Shavasana())
+
+def do_load_workouts():
+    return [DefaultWorkout]
