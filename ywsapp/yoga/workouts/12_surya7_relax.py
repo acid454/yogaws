7738b5715303ae903116e9d9c2c4ddeba1c3c753
Dmitry Repnikov <acid454@gmail.com>
2025-05-09 16:05:41 +0300

    update workouts

--- ywsapp/yoga/workouts/12_surya7_relax.py
+++ ywsapp/yoga/workouts/12_surya7_relax.py
@@ -24,17 +24,18 @@ class DefaultWorkout(BaseWorkout):
         self.sets.append(SuryaNamaskar(timings = 'slow', cnt = 3))
         self.sets.append(SuryaNamaskar(timings = 'fast', cnt = 4))
 
+        #self.wrap_asana(Asanas.gorka.GorkaNormal(tm_main = 10, tm_prepare = 5))
+        #self.wrap_asana(Asanas.parivritta.ParivrittaLeft())
+        #self.wrap_asana(Asanas.gorka.GorkaBase(tm_main = 10))
+        #self.wrap_asana(Asanas.parivritta.ParivrittaRight())
+        #self.wrap_asana(Asanas.gorka.GorkaBase(tm_main = 10))
         self.wrap_asana(Asanas.gorka.GorkaNormal(tm_main = 10, tm_prepare = 5))
-        self.wrap_asana(Asanas.parivritta.ParivrittaLeft())
-        self.wrap_asana(Asanas.gorka.GorkaBase(tm_main = 10))
-        self.wrap_asana(Asanas.parivritta.ParivrittaRight())
-        self.wrap_asana(Asanas.gorka.GorkaBase(tm_main = 10))
         self.wrap_asana(Asanas.padottanasana.PadottanasanaLeft())
         self.wrap_asana(Asanas.padottanasana.PadottanasanaRight())
         #self.wrap_asana(Asanas.prasarita_padottanasana.PrasaritaPadottanasana(with_hands = False))
 
         self.wrap_asana(Asanas.short_poses.Seli())
-        self.wrap_asana(Asanas.baddha_konasana.BaddhaKonasana(with_knees = False))
+        self.wrap_asana(Asanas.baddha_konasana.BaddhaKonasana(with_knees = True))
 
         self.wrap_asana(Asanas.gorka.GorkaNormal(tm_main = 45, tm_prepare = 5))
         self.wrap_asana(Asanas.kobra.KobraWithRotations())
