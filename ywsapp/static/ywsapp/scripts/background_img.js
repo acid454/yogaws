e3f107d9c9b8e7ab19a123a5a31cfe87dead5e74
Dmitry Repnikov <acid454@gmail.com>
2025-05-09 18:20:44 +0300

    bg image setupt moved to separate js file

---
+++ ywsapp/static/ywsapp/scripts/background_img.js
@@ -0,0 +1,9 @@
+function setup_background_image(res_path) {
+    background_img_id = sessionStorage.getItem('background_img_id');
+    if (background_img_id == null) {
+        background_img_id = "{{main_bg_image}}";
+        sessionStorage.setItem('background_img_id', background_img_id);
+    }
+    document.getElementById('main_centered_display').style.backgroundImage = "url('" + res_path + background_img_id + "')";
+    document.getElementById('main_workout_list_display').style.display = 'none';
+}
