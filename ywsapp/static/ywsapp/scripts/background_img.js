f8e2b49a2dcb6d9dbd46baec8a40849401e9425c
Dmitry Repnikov <acid454@gmail.com>
2025-05-12 12:04:52 +0300

    fixed main bg image template/js

--- ywsapp/static/ywsapp/scripts/background_img.js
+++ ywsapp/static/ywsapp/scripts/background_img.js
@@ -1,7 +1,7 @@
-function setup_background_image(res_path) {
+function setup_background_image(res_path, main_bg_image) {
     background_img_id = sessionStorage.getItem('background_img_id');
     if (background_img_id == null) {
-        background_img_id = "{{main_bg_image}}";
+        background_img_id = main_bg_image;
         sessionStorage.setItem('background_img_id', background_img_id);
     }
     document.getElementById('main_centered_display').style.backgroundImage = "url('" + res_path + background_img_id + "')";
