83b4cc0bc6c8704cae3697027290dca1dc159ba2
Dmitry Repnikov <acid454@gmail.com>
2025-05-14 18:08:36 +0300

    shavasana acting settings

--- ywsapp/yoga/asanas/shavasana.py
+++ ywsapp/yoga/asanas/shavasana.py
@@ -9,7 +9,6 @@
 from base import BaseAsana, BaseTask
 from properties import IntProperty
 from metronomes import MetronomeShavasana
-from snd_pools import SND_ZAKONCHILI_DALSHE
 from integer_constants import ShavasanaActing
 
 
@@ -23,22 +22,20 @@ class Shavasana(BaseAsana):
             metronome=MetronomeShavasana(),
             images=[f"shavasana{x}" for x in range(1,6)]
         ))
-        self.pool("name").append("descr_shavasana1")
-        self.pool("name").append("descr_shavasana2")
-        self.pool("name").append("descr_shavasana3")
-        self.pool("name").append("descr_shavasana4")
-        self.pool("name").append("descr_shavasana5")
     
     def build(self, workout, _set):
         super().build(workout, _set)
 
-        #if workout.user.shavasana_acting == 
-
-        # {% if not glob.get('last_before_shavasana') %}
-        #end_pool = workout.sets[-2].asanas[-1].tasks[-1].pool("end")
         end_pool = workout.prev_item(self).pool("end")
         end_pool.remove("i_dvishemsia_dalshe1")
         end_pool.remove("i_dvishemsia_dalshe2")
         end_pool.remove("i_dvishemsia_dalshe3")
 
-
+        if workout.user is None or workout.user.shavasana_acting != ShavasanaActing.NEVER_ACTING:
+            snd_mandatory = True if workout.user is None else (workout.user.shavasana_acting == ShavasanaActing.ALWAYS_ACTING)
+            self.pool("name").append("descr_shavasana1", mandatory = snd_mandatory)
+            self.pool("name").append("descr_shavasana2", mandatory = snd_mandatory)
+            self.pool("name").append("descr_shavasana3", mandatory = snd_mandatory)
+            self.pool("name").append("descr_shavasana4", mandatory = snd_mandatory)
+            self.pool("name").append("descr_shavasana5", mandatory = snd_mandatory)
+        
