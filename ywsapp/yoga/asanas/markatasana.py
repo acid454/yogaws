051e9c4f8d3811c045da7b8fd5fa4b7147532fda
Dmitry Repnikov <acid454@gmail.com>
2025-07-16 12:00:26 +0300

    fixed markatasana cycles

--- ywsapp/yoga/asanas/markatasana.py
+++ ywsapp/yoga/asanas/markatasana.py
@@ -25,9 +25,10 @@ class Markatasana(BaseAsana):
             caption=self.caption + " (подготовка)",
             property=self.tm_enter,
             metronome=MetronomeRest(),
-            images=["markatasana base"]
+            images=["markatasana_base1", "markatasana_base2"]
         ))
         self.pool("name").append("name_markatasana_delaem_markatasanu")
+        self.pool("name").append("name_markatasana_variant")
         self.pool("continue").append("enter_markatasana")
 
         for i in range(self.cycles_count.value):
@@ -35,11 +36,14 @@ class Markatasana(BaseAsana):
                 caption=self.caption + "\nвлево",
                 property=self.tm_main,
                 metronome=MetronomeWork(),
-                images=[f"markatasana_left{j}" for j in range(1,3)]
+                images=[f"markatasana_left{j}" for j in range(1,4)]
             ))
 
             if i == 0:
                 self.pool("continue").append("descr_markatasana_left")
+            else:
+                self.pool("float").append("descr_markatasana_zatragivaet_tazobedrennue", overlapse = True)
+
             self.pool("end").append(SND_MENIAJEM_NOGI)
             self.pool("end").append("i potom rovno na oborot")
             
@@ -47,7 +51,7 @@ class Markatasana(BaseAsana):
                 caption=self.caption + "\nвправо",
                 property=self.tm_main,
                 metronome=MetronomeWork(),
-                images=[f"markatasana_right{j}" for j in range(1,3)]
+                images=[f"markatasana_right{j}" for j in range(1,4)]
             ))
             self.pool("end").append(SND_MENIAJEM_NOGI)
             self.pool("end").append("i potom rovno na oborot")
@@ -93,7 +97,7 @@ class MarkatasanaWithLegs(Markatasana):
         self.pool("end").append("i_potianulis'_vlevo")
 
         stopi_do_kovrika_rand = random.randint(1, self.cycles_twist.value - 1) + random.randint(0,1)/2
-        for i in range(self.cycles_count.value):
+        for i in range(self.cycles_twist.value):
             self.tasks.append(BaseTask(
                 caption=self.caption + "\nвлево",
                 property=self.tm_main,
